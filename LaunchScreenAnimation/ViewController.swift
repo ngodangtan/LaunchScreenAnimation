350115d06d83519558d15e20588e9ca932446eca
27293 <myltd6@fpt.com.vn>
2020-11-20 11:06:51 +0700

    Initial Commit

---
+++ LaunchScreenAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaunchScreenAnimation
+//
+//  Created by Ngo Dang tan on 20/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
